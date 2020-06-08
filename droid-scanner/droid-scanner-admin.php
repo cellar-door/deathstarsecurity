5491047fe5da418deef299c165391daadd542d54
Yordan Soares <contacto@yordansoar.es>
2020-06-08 19:58:24 -0400

    Added i18n feature and .pot file to all three plugins
    1. Added internationalization feature to:
    - Storm Trooper Customer Targeting
    - Droid Scanner
    - Exhaust Port SEO
    2. Added translation template files to the three plugins ({plugin}/languages/).

--- droid-scanner/droid-scanner-admin.php
+++ droid-scanner/droid-scanner-admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Admin page for Droid Scanner.
  *
@@ -6,38 +7,40 @@
  */
 
 // Don't call the file directly and give up info!
-if ( ! function_exists( 'add_action' ) ) {
-	echo 'Lock it up!';
+if (!function_exists('add_action')) {
+	echo __('Lock it up!', 'droid-scanner');
 	exit;
 }
 
 require_once DROIDSCANNER__PLUGIN_DIR . '/class-droid-scanner-list.php';
 
-add_action( 'admin_menu', 'droidscanner_admin_menu' );
+add_action('admin_menu', 'droidscanner_admin_menu');
 
 /**
  * Create our admin pages and put them into the admin menu.
  */
-function droidscanner_admin_menu() {
-	add_menu_page( __( 'Droid Scanner', 'droidscanner' ), __( 'Droid Scanner', 'droidscanner' ), 'publish_posts', 'droidscanner', 'droidscanner_table' );
+function droidscanner_admin_menu()
+{
+	add_menu_page(__('Droid Scanner', 'droid-scanner'), __('Droid Scanner', 'droid-scanner'), 'publish_posts', 'droidscanner', 'droidscanner_table');
 }
 
 /**
  * Create a table of all the Droid Scanner Entries.
  */
-function droidscanner_table() {
+function droidscanner_table()
+{
 
 	$droidscanner_table = new Droid_Scanner_List();
 	$droidscanner_table->prepare_items();
-	?>
+?>
 	<div class="wrap">
 
-			<h1>Droid Scanner Entries:</h1>
-				<p> Below is a list of the entries from the Droid Scanner Forms. </p>
-				<form id="droidscanner-table" method="post">
-			<input type="hidden" name="page" value="<?php echo isset( $_REQUEST['page'] ) ? intval( $_REQUEST['page'] ) : ''; ?>" />
+		<h1><?php _e('Droid Scanner Entries:', 'droid-scanner'); ?></h1>
+		<p><?php _e('Below is a list of the entries from the Droid Scanner Forms.', 'droid-scanner'); ?></p>
+		<form id="droidscanner-table" method="post">
+			<input type="hidden" name="page" value="<?php echo isset($_REQUEST['page']) ? intval($_REQUEST['page']) : ''; ?>" />
 			<?php $droidscanner_table->display(); ?>
-				</form>
+		</form>
 	</div>
-	<?php
+<?php
 }
